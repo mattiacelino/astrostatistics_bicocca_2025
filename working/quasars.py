738602e2ec5e68eba19a15d437fc71f4ae519cfd
mattiacelino <m.celino@campus.unimib.it>
2025-03-22 22:32:10 +0100

    Lesson 3 - finished rejection sampling

--- working/quasars.py
+++ working/quasars.py
@@ -7,19 +7,21 @@ import scipy
 data = fetch_dr7_quasar()
 
 # select the first 10000 points
-data = data[:10000]
+#data = data[:10000]
 
 z = data['redshift']
-plt.hist(z, bins=30, histtype='step', density=True)
-plt.show()
+plt.hist(z, bins=30, histtype='step', density=True, label="original datas")
+#plt.show()
 
 # With rejecton sampling
-disth = scipy.stats.rv_histogram((counts,bins)) # serve per prendere la distribuzione
+counts, bins = np.histogram(z, bins=30, density=True)
+disth = scipy.stats.rv_histogram((counts, bins))  # serve per prendere la distribuzione
 
 N = 1000
 x = np.random.uniform(0,6, N)
-y = np.random.uniform(0,1040, N)
+y = np.random.uniform(0,np.max(counts), N)
 
-goodpoints = x[y<np.histogram(z, bins=30, density=True)]
-plt.hist(goodpoints, bins=30, histtype='step', density=True)
+goodpoints = x[y <= disth.pdf(x)]
+plt.hist(goodpoints, bins=30, histtype='step', density=True, label="rejection sampling")
+plt.legend()
 plt.show()
