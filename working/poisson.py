51c2c4ec1ea644751d131f6ee716d05527ea2a9e
mattiacelino <m.celino@campus.unimib.it>
2025-03-22 11:51:26 +0100

    Lesson 2 - MonteCarlo and Poisson 2.0

--- working/poisson.py
+++ working/poisson.py
@@ -1,10 +1,18 @@
 import numpy as np
+import math
 from matplotlib import pyplot as plt
-import scipy.stats
+from scipy.optimize import curve_fit
 
 deaths = np.array([0,    1,  2, 3, 4])
 groups = np.array([109, 65, 22, 3, 1])
 frequency = groups / groups.sum()
 
+def poisson(k, mu):
+    return (mu**k * np.exp(-mu)) / k
+
+(args, var) = curve_fit(poisson, deaths, frequency, absolute_sigma=True)
+t = np.linspace(deaths.min(), deaths.max(), 1000)
 plt.plot(deaths, frequency, marker="^", color="dodgerblue", ls="")
+plt.plot(t, poisson(t, *args), ls="--", color="blue", label="fit")
+plt.legend()
 plt.show()
