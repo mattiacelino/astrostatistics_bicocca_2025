812c245f086d5b7d668af82f258b2fb56ca21b92
mattiacelino <m.celino@campus.unimib.it>
2025-03-22 18:46:45 +0100

    Lesson 3

---
+++ working/quasars.py
@@ -0,0 +1,25 @@
+from astroML.datasets import fetch_dr7_quasar
+from matplotlib import pyplot as plt
+import numpy as np
+import scipy
+
+# Fetch the quasar data
+data = fetch_dr7_quasar()
+
+# select the first 10000 points
+data = data[:10000]
+
+z = data['redshift']
+plt.hist(z, bins=30, histtype='step', density=True)
+plt.show()
+
+# With rejecton sampling
+disth = scipy.stats.rv_histogram((counts,bins)) # serve per prendere la distribuzione
+
+N = 1000
+x = np.random.uniform(0,6, N)
+y = np.random.uniform(0,1040, N)
+
+goodpoints = x[y<np.histogram(z, bins=30, density=True)]
+plt.hist(goodpoints, bins=30, histtype='step', density=True)
+plt.show()
